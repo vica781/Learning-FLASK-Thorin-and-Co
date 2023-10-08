c2169b8895eab094565576d0948d1f88faf617ed
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-10-08 22:36:30 +0100

    Pass data from a view to a template

--- run.py
+++ run.py
@@ -7,22 +7,22 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", page_title="Home")
 
 
 @app.route("/about")
 def about():
-    return render_template("about.html")
+    return render_template("about.html", page_title="About")
 
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")
+    return render_template("contact.html", page_title="Contact")
 
 
 @app.route("/careers")
 def careers():
-    return render_template("careers.html")
+    return render_template("careers.html", page_title="Careers")
 
 
 if __name__ == "__main__":
