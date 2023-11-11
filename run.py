0bae95d16960daf6b2028581fe2395b69026c456
Victoria Kalenik-Harris <vk781@hotmail.com>
2023-11-11 18:55:26 +0000

    Link member's name to url with info

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html", page_title="Home")
+    return render_template("index.html")
 
 
 @app.route("/about")
@@ -17,6 +17,17 @@ def about():
     with open("data/company.json", "r") as json_data:
         data = json.load(json_data)
     return render_template("about.html", page_title="About", company=data)
+
+
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
 
 
 @app.route("/contact")
